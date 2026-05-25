10f0f08025589ba15c1c9b2556a629973f453aed
Kent Overstreet <kent.overstreet@linux.dev>
2026-05-25 10:18:31 -0400

    btree locking: kill unused ip arg

--- fs/btree/cache.c
+++ fs/btree/cache.c
@@ -1029,7 +1029,7 @@ static noinline struct btree *bch2_btree_node_fill(struct btree_trans *trans,
 	 * been freed:
 	 */
 	if (path) {
-		int ret = bch2_btree_path_relock(trans, path, _THIS_IP_);
+		int ret = bch2_btree_path_relock(trans, path);
 		if (ret)
 			return ERR_PTR(ret);
 	}
@@ -1097,7 +1097,7 @@ static noinline struct btree *bch2_btree_node_fill(struct btree_trans *trans,
 	 */
 	if (path) {
 		int ret = bch2_trans_relock(trans) ?:
-			  bch2_btree_path_relock(trans, path, _THIS_IP_);
+			  bch2_btree_path_relock(trans, path);
 		if (ret) {
 			if (b)
 				six_unlock_type(&b->c.lock, lock_type);
@@ -1250,7 +1250,7 @@ retry:
 		bch2_btree_node_wait_on_read(trans, b);
 
 		ret =   bch2_trans_relock(trans) ?:
-			bch2_btree_path_relock(trans, path, _THIS_IP_);
+			bch2_btree_path_relock(trans, path);
 		if (ret)
 			return ERR_PTR(ret);
 
